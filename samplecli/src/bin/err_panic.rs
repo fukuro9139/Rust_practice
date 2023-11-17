b44145acaba6d23b3589750ebe5588a574cd6e80
fukuro9139 <peridot1451@gmail.com>
2023-11-18 02:11:48 +0900

    diplay, from

--- samplecli/src/bin/err_panic.rs
+++ samplecli/src/bin/err_panic.rs
@@ -1,25 +1,43 @@
 fn get_int_from_file() -> Result<i32, MyError> {
 	let path = "number.txt";
 
-	let num_str = std::fs::read_to_string(path).map_err(|e| MyError::IO(e))?;
+	let num_str = std::fs::read_to_string(path).map_err(MyError::from)?;
 	num_str
 		.trim()
 		.parse::<i32>()
 		.map(|t| t * 2)
-		.map_err(|e| MyError::Num(e))
+		.map_err(MyError::from)
 }
 
 fn main() {
 	match get_int_from_file() {
 		Ok(x) => println!("{}", x),
-		Err(e) => match e {
-			MyError::IO(cause) => println!("IO Error: {}", cause),
-			MyError::Num(cause) => println!("Parse Error: {}", cause),
-		},
+		Err(e) => println!("{}", e),
 	}
 }
 
 enum MyError {
 	IO(std::io::Error),
 	Num(std::num::ParseIntError),
+}
+
+impl From<std::io::Error> for MyError {
+	fn from(cause: std::io::Error) -> Self {
+		MyError::IO(cause)
+	}
+}
+
+impl From<std::num::ParseIntError> for MyError {
+	fn from(cause: std::num::ParseIntError) -> Self {
+		MyError::Num(cause)
+	}
+}
+
+impl std::fmt::Display for MyError {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+		match self {
+			MyError::IO(cause) => write!(f, "IO Error: {}", cause),
+			MyError::Num(cause) => write!(f, "Parse Error: {}", cause),
+		}
+	}
 }
