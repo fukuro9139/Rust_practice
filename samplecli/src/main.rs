062f5d2e3dde27cfbca63536367c6545e07b2e8b
fukuro9139 <peridot1451@gmail.com>
2023-11-17 23:17:43 +0900

    clap Builder

--- samplecli/src/main.rs
+++ samplecli/src/main.rs
@@ -1,6 +1,20 @@
-use std::env;
+use clap::{App, arg};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-	println!("{:?}", args);
+    let matches = App::new("My RPN program")
+		.version("1.0.0")
+		.author("Kengo Fukunaga")
+		.about("Super awesome sample RPN calculator")
+		.arg(arg!([FILE] "Formula written in RPN").required(false))
+		.arg(arg!(-v --verbose ... "Sets the level of verbosity").required(false))
+		.get_matches();
+	
+	match matches.value_of("FILE") {
+		Some(file) => println!("File specified: {}", file),
+		None => println!("No file specified."),
+	}
+
+	let verbose = matches.is_present("verbose");
+	println!("Is verbosity specified?: {}", verbose);
+
 }
