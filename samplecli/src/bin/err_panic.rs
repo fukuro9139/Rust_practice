c1703c5a74714fdabdbf15febca27f6ebb554031
fukuro9139 <peridot1451@gmail.com>
2023-11-18 20:11:48 +0900

    anyhow

--- samplecli/src/bin/err_panic.rs
+++ samplecli/src/bin/err_panic.rs
@@ -1,43 +1,21 @@
-fn get_int_from_file() -> Result<i32, MyError> {
+use anyhow::{Context, Result};
+
+fn get_int_from_file() -> Result<i32> {
 	let path = "number.txt";
 
-	let num_str = std::fs::read_to_string(path).map_err(MyError::from)?;
+	let num_str = std::fs::read_to_string(path)
+		.with_context(|| format!("failed to open path: {}", path))?;
+
 	num_str
 		.trim()
 		.parse::<i32>()
 		.map(|t| t * 2)
-		.map_err(MyError::from)
+		.context("failed to parse string")
 }
 
 fn main() {
 	match get_int_from_file() {
 		Ok(x) => println!("{}", x),
-		Err(e) => println!("{}", e),
-	}
-}
-
-enum MyError {
-	IO(std::io::Error),
-	Num(std::num::ParseIntError),
-}
-
-impl From<std::io::Error> for MyError {
-	fn from(cause: std::io::Error) -> Self {
-		MyError::IO(cause)
-	}
-}
-
-impl From<std::num::ParseIntError> for MyError {
-	fn from(cause: std::num::ParseIntError) -> Self {
-		MyError::Num(cause)
-	}
-}
-
-impl std::fmt::Display for MyError {
-	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-		match self {
-			MyError::IO(cause) => write!(f, "IO Error: {}", cause),
-			MyError::Num(cause) => write!(f, "Parse Error: {}", cause),
-		}
+		Err(e) => println!("{:#?}", e),
 	}
 }
